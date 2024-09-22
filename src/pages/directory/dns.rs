daf0d08baea003972e1e9e260d55fd9a7b35b14b
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-09-22 09:47:01 +0200

    Fix style (#22)
    Font mono
    Text no wrap

--- src/pages/directory/dns.rs
+++ src/pages/directory/dns.rs
@@ -196,7 +196,7 @@ pub fn DnsDisplay() -> impl IntoView {
                                         </h2>
                                     </div>
                                     <textarea
-                                        class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                        class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm font-mono text-nowrap focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                                         readonly=true
                                         rows=20
                                         prop:value=zonefile
